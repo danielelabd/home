42793a4093b8857d3027d4a52d4d2fce6052a255
Daniel Elabd <danielh.elabd@gmail.com>
2021-07-21 21:33:39 +0100

    Updates

---
+++ precache-manifest.895c2b754a8bdee83ca06d57fe1ce17f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58c2e36087ddd06103a8dc1d7d3c15d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b45f1258182a38ee1db6",
+    "url": "/home/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "a8f48d75cafbf94f1fe5",
+    "url": "/home/static/css/main.77a26927.chunk.css"
+  },
+  {
+    "revision": "b45f1258182a38ee1db6",
+    "url": "/home/static/js/2.00447eea.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.00447eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f48d75cafbf94f1fe5",
+    "url": "/home/static/js/main.d16682ad.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "483773fe65e88af29b9cc51b1f356b5a",
+    "url": "/home/static/media/Thesis.483773fe.pdf"
+  },
+  {
+    "revision": "803f78eebd733d5ef8106c351f4477ed",
+    "url": "/home/static/media/diptrace.803f78ee.png"
+  },
+  {
+    "revision": "ad6454de56dbb18fd0f6d055ae3824f0",
+    "url": "/home/static/media/floboat.ad6454de.jpg"
+  },
+  {
+    "revision": "da5037ab9e93e04390acba4efbb5dc02",
+    "url": "/home/static/media/imperial.da5037ab.jpg"
+  },
+  {
+    "revision": "1d33781a2000891fbb7c3a5437b4b83f",
+    "url": "/home/static/media/linkwhere.1d33781a.png"
+  },
+  {
+    "revision": "b647e25c8adcf6247faff033f55a9566",
+    "url": "/home/static/media/multi.b647e25c.png"
+  },
+  {
+    "revision": "785229cb260dd490f1b83a8ff7834d26",
+    "url": "/home/static/media/pcb.785229cb.png"
+  },
+  {
+    "revision": "99127944971eaa826158d7beb88990b5",
+    "url": "/home/static/media/profilepic.99127944.png"
+  },
+  {
+    "revision": "239809b3bd5f3417409eea3c0c668304",
+    "url": "/home/static/media/resume.239809b3.pdf"
+  },
+  {
+    "revision": "24a83a364152a163dccb2a206a793880",
+    "url": "/home/static/media/smartsystems.24a83a36.png"
+  },
+  {
+    "revision": "4fab8eeac8da618e10af675b4ee8d991",
+    "url": "/home/static/media/ss.4fab8eea.jpg"
+  },
+  {
+    "revision": "84b71acb3ede239407e3a7c5eab83bbc",
+    "url": "/home/static/media/tuplast.84b71acb.jpg"
+  }
+]);

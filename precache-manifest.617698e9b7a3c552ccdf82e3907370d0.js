8e38b801a21cf8265d90386816ce82f3bf097c0e
danielelabd <danielh.elabd@gmail.com>
2022-06-16 13:19:54 +0200

    Updates

---
+++ precache-manifest.617698e9b7a3c552ccdf82e3907370d0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7fa0582e2fb732985932c465835a65",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5096b19950d14ab20efd",
+    "url": "/home/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "1677ca12dafcf1e54c36",
+    "url": "/home/static/css/main.842a5b58.chunk.css"
+  },
+  {
+    "revision": "5096b19950d14ab20efd",
+    "url": "/home/static/js/2.a3103720.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.a3103720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1677ca12dafcf1e54c36",
+    "url": "/home/static/js/main.7c2dd5e7.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "483773fe65e88af29b9cc51b1f356b5a",
+    "url": "/home/static/media/bengthesis.483773fe.pdf"
+  },
+  {
+    "revision": "ad6454de56dbb18fd0f6d055ae3824f0",
+    "url": "/home/static/media/floboat.ad6454de.jpg"
+  },
+  {
+    "revision": "da5037ab9e93e04390acba4efbb5dc02",
+    "url": "/home/static/media/imperial.da5037ab.jpg"
+  },
+  {
+    "revision": "84b122208301328332652f15528b0684",
+    "url": "/home/static/media/marses.84b12220.png"
+  },
+  {
+    "revision": "992f504c21c42d6be2a5b334eaa5f748",
+    "url": "/home/static/media/mscthesis.992f504c.pdf"
+  },
+  {
+    "revision": "a0bf6d6945f802eac56d6413a9f410ad",
+    "url": "/home/static/media/otherprojects.a0bf6d69.png"
+  },
+  {
+    "revision": "785229cb260dd490f1b83a8ff7834d26",
+    "url": "/home/static/media/pcb.785229cb.png"
+  },
+  {
+    "revision": "99127944971eaa826158d7beb88990b5",
+    "url": "/home/static/media/profilepic.99127944.png"
+  },
+  {
+    "revision": "36a323da61ddef8e3d6f4e5bdef43825",
+    "url": "/home/static/media/resume.36a323da.pdf"
+  },
+  {
+    "revision": "368100cdee53d2be4ccef7110f19b10a",
+    "url": "/home/static/media/sensor.368100cd.gif"
+  },
+  {
+    "revision": "e9361a553fdf6c7561fcb374fb4a7779",
+    "url": "/home/static/media/smart.e9361a55.png"
+  },
+  {
+    "revision": "4fab8eeac8da618e10af675b4ee8d991",
+    "url": "/home/static/media/ss.4fab8eea.jpg"
+  },
+  {
+    "revision": "9a1d54eb24b350f50d17e7035fb31502",
+    "url": "/home/static/media/tuplast.9a1d54eb.png"
+  }
+]);
